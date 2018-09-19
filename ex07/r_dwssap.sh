b46f527c12e5a4ce8759bbed26250678a12f8ba1
Chenna Kautilya <kchenna@student.42.us.org>
2018-09-18 18:16:31 -0700

    completed day01

--- ex07/r_dwssap.sh
+++ ex07/r_dwssap.sh
@@ -1,1 +1,1 @@
-cat /etc/passwd | sed '/^#/ d' | awk "NR%2==0" | awk -F  ":" '{print $1}' | rev | awk -v ORS=', ' "NR>=$FT_LINE1 && NR<=$FT_LINE2" | sed 's/, $/\n/' | tr '\n' .
+cat /etc/passwd | sed '/^#/ d' | awk "NR%2==0" | awk -F  ":" '{print $1}' | rev | sort -r | awk -v ORS=', ' "NR>=$FT_LINE1 && NR<=$FT_LINE2" | sed 's/, $/\n/' | tr '\n' .
