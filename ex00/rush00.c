4d022c50890a88ffb019ae30b957991edab4c49c
Chenna Kautilya <kchenna@student.42.us.org>
2018-09-23 20:54:40 -0700

    split function into two seperate up and down functions to avoid long lines

--- ex00/rush00.c
+++ ex00/rush00.c
@@ -6,11 +6,29 @@
 /*   By: kchenna <kchenna@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/23 11:17:40 by eubotnar          #+#    #+#             */
-/*   Updated: 2018/09/23 12:46:57 by kchenna          ###   ########.fr       */
+/*   Updated: 2018/09/23 20:49:04 by kchenna          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	ft_putchar(char c);
+
+void	down(int i, int w, int h)
+{
+	if (i > w && i <= w * (h - 1))
+		ft_putchar('|');
+	else
+		ft_putchar('o');
+}
+
+void	side(int i, int w, int h)
+{
+	if (i > 1 && i < w)
+		ft_putchar('-');
+	else if (i >= w * h - w + 1 && i < w * h)
+		ft_putchar('-');
+	else
+		ft_putchar(' ');
+}
 
 void	rush(int w, int h)
 {
@@ -21,19 +39,11 @@ void	rush(int w, int h)
 	{
 		if (i % w == 0 || i % w == 1 || i == 1)
 		{
-			if (i > w && i <= w * (h - 1))
-				ft_putchar('|');
-			else
-				ft_putchar('o');
+			down(i, w, h);
 		}
 		else
 		{
-			if (i > 1 && i < w)
-				ft_putchar('-');
-			else if (i >= w * h - w + 1 && i < w * h)
-				ft_putchar('-');
-			else
-				ft_putchar(' ');
+			side(i, w, h);
 		}
 		if (i % w == 0)
 			ft_putchar('\n');
