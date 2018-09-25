c62224b59d43fb7208349cd935054fe5c4abc784
Chenna Kautilya <kchenna@student.42.us.org>
2018-09-24 19:36:41 -0700

    added ft_putnbr

--- ex01/ft_putnbr.c
+++ ex01/ft_putnbr.c
@@ -6,15 +6,32 @@
 /*   By: kchenna <kchenna@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/24 16:45:43 by kchenna           #+#    #+#             */
-/*   Updated: 2018/09/24 17:01:35 by kchenna          ###   ########.fr       */
+/*   Updated: 2018/09/24 19:36:22 by kchenna          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <unistd.h>
+void	ft_putchar(char c);
 
-void ft_putchar(char c)
+void	ft_putnbr(int nb)
 {
-	write(1, &c, 1);
+	if (nb == -__INT_MAX__)
+	{
+		ft_putchar('-');
+		ft_putchar('2');
+		ft_putnbr(147483647);
+		return ;
+	}
+	else if (nb < 0)
+	{
+		nb = -nb;
+		ft_putchar('-');
+		ft_putnbr(nb);
+	}
+	else if (nb >= 10)
+	{
+		ft_putnbr(nb / 10);
+		ft_putnbr(nb % 10);
+	}
+	else
+		ft_putchar(nb + 48);
 }
-
-
