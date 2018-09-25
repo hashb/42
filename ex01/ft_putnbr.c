4dc53a716fa53f4c2cbbcf22b4ec34985f87fca7
Chenna Kautilya <kchenna@student.42.us.org>
2018-09-25 14:38:43 -0700

    testing towel

--- ex01/ft_putnbr.c
+++ ex01/ft_putnbr.c
@@ -6,7 +6,7 @@
 /*   By: kchenna <kchenna@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/24 16:45:43 by kchenna           #+#    #+#             */
-/*   Updated: 2018/09/24 19:36:22 by kchenna          ###   ########.fr       */
+/*   Updated: 2018/09/25 14:38:06 by kchenna          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,24 +14,24 @@ void	ft_putchar(char c);
 
 void	ft_putnbr(int nb)
 {
-	if (nb == -__INT_MAX__)
-	{
-		ft_putchar('-');
-		ft_putchar('2');
-		ft_putnbr(147483647);
-		return ;
-	}
-	else if (nb < 0)
-	{
-		nb = -nb;
-		ft_putchar('-');
-		ft_putnbr(nb);
-	}
-	else if (nb >= 10)
-	{
-		ft_putnbr(nb / 10);
-		ft_putnbr(nb % 10);
-	}
-	else
-		ft_putchar(nb + 48);
+	// if (nb == -__INT_MAX__)
+	// {
+	// 	// ft_putchar('-');
+	// 	ft_putchar('2');
+	// 	ft_putnbr(147483647);
+	// 	return ;
+	// }
+	// if (nb < 0)
+	// {
+	// 	nb = -nb;
+	// 	// ft_putchar('-');
+	// 	// ft_putnbr(nb);
+	// }
+	// if (nb >= 10)
+	// {
+	// 	ft_putnbr(nb / 10);
+	// 	ft_putnbr(nb % 10);
+	// }
+	// else
+	// 	ft_putchar(nb + 48);
 }
