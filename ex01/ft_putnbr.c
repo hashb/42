0ff19c7e6fcafc5182b85335031f0ec383d7186d
Kautilya Chenna <kchenna@e1z1r4p39.42.us.org>
2018-09-25 20:35:38 -0700

    fixing ft_putnbr

--- ex01/ft_putnbr.c
+++ ex01/ft_putnbr.c
@@ -6,21 +6,30 @@
 /*   By: kchenna <kchenna@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/24 16:45:43 by kchenna           #+#    #+#             */
-/*   Updated: 2018/09/25 20:29:13 by kchenna          ###   ########.fr       */
+/*   Updated: 2018/09/25 20:34:57 by kchenna          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	ft_putchar(char c);
 
+void	ft_putnbr_spl(int nb)
+{
+	if (nb < 0)
+	{
+		ft_putchar('-');
+		ft_putchar('2');
+		ft_putnbr(147483648);
+	}
+	ft_putchar('2');
+	ft_putnbr(147483647);
+	return ;
+}
+
 void	ft_putnbr(int nb)
 {
 	if (nb == -__INT_MAX__ || nb == __INT_MAX__)
 	{
-		if (nb < 0)
-			ft_putchar('-');
-		ft_putchar('2');
-		ft_putnbr(147483647);
-		return ;
+		ft_putnbr_spl(nb);
 	}
 	if (nb < 0)
 	{
